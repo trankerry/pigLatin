5f830ff436f76c793f7479f2b141c1002853f97e
Alex Martinez <alexkmartinez77@gmail.com>
2022-04-22 00:08:18 -0700

    Polish

--- piglatin.js
+++ piglatin.js
@@ -1,21 +1,21 @@
 /*
-Write a function called piglatin that is given a string representing a word and expected
+Write a function called piglatin whose input is a string representing a word and is expected
 to translate that word into Pig Latin and return that translated string back.
 
 - For words that begin with consonants: 
-  All letters before the initial vowel are placed at the end of the word sequence. Then, "ay" is added.
-  "y at the beginning of a word is considered a consonant sound
-  "y" is otherwise considered a vowel
+  All letters before the initial vowel are placed at the end of the word sequence. 
+  Then, "ay" is added.
+  - "y" at the beginning of a word is considered a consonant sound
+  - "y" is otherwise considered a vowel
 
-- For Words that begin with consonant clusters (multiple consonants that form one sound):
-  The whole sound is added to the end when speaking or writing
-  "qu" is considered a consonant sound
+- For Words that begin with consonant clusters (multiple consonants that form one sound),
+  the whole consonant cluster is moved to the end of the word and then "ay" is added.
+  - "qu" is considered a consonant cluster
 
-- For words that begin with vowel sounds
-  Add "way" to the end
+- For words that begin with vowels add "way" to the end
 
   Examples: 
-
+      ---------------------------
       word: Pig Latin Translation
       ---------------------------
       hello: "ellohay",
